03b516a16bd3d400b3fbcee2b51e956dc49dc3cf
BeanEater1269 <104499049+BeanEater1269@users.noreply.github.com>
2026-02-06 17:26:38 +0800

    Implementeed Speech to Text

--- Main/main.py
+++ Main/main.py
@@ -59,7 +59,7 @@ def button_press(key):
     if key == keyboard.Key.esc:
         print("Closing Program")
         return False
-
+    is_busy = False
     try:
         if key.char == "f":
             print("Describe Webpage")
@@ -79,10 +79,29 @@ def button_press(key):
             Thread(target=converter.textToSpeech, args=(desc['<OCR>'],)).start() 
             extract_feature.scan_standard_chrome()
 
-        if key.char == "j":
-            print("Execute Action triggered...")
-            # START IN A THREAD so the listener doesn't freeze
-            Thread(target=execute_typed_action).start()
+        if hasattr(key, 'char') and key.char == "j":
+            if is_busy:
+                print("Iris is already working, please wait...")
+            else:
+                print("Execute Action triggered...")
+
+                def task():
+                    text = converter.speechToText()
+                    print(f"Captured: {text}")
+                    global is_busy
+                    is_busy = True
+                    try:
+                        if text:
+                            print(f"User wants to: {text}")
+                            target_id = ID_selector.select_id_semantically(text,EmbeddingModel)
+
+                            if target_id is not None:
+                                interaction.smart_interact(target_id)
+                            else:
+                                print("No matching element found.")
+                    finally:
+                        is_busy = False
+                Thread(target=task, daemon=True).start()
 
     except AttributeError:
         pass
