8f61382b6c3e5c6fa27c8f46e03f93df95e9bdca
BeanEater1269 <104499049+BeanEater1269@users.noreply.github.com>
2026-02-06 18:15:44 +0800

    Implementeed Speech to Text

--- Main/converter.py
+++ Main/converter.py
@@ -12,7 +12,6 @@ def speechToText():
         print("Listening...")
         recognizer.adjust_for_ambient_noise(source, duration=1)
         audio = recognizer.listen(source, timeout=5, phrase_time_limit=5)
-        audio = recognizer.listen(source)
     text = recognizer.recognize_google(audio)
     print("Recognized:", text)
     return text
